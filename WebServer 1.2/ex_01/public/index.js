32784416b20f11c0d9bda36af4bde4044deedb69
Aidan <aidan.duong@gmx.de>
2022-04-21 19:55:55 +0200

    Connecting the WebApp with the WebServer fixed

--- WebServer 1.2/ex_01/public/index.js
+++ WebServer 1.2/ex_01/public/index.js
@@ -7,8 +7,7 @@ function print_hello_world() {
 // this is working because of the import in the html file
 // https://socket.io/docs/v4/client -installation/#standalone -build
 
-
-const socket = io
+const socket = io()
 
 socket.on("connect", () => {
   console.log("Connected to the webserver.")
